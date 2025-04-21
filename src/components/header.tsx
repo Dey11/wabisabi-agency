54e4be2e5b41dbc42a698ff2a518300d4f221cf0
Dey11 <shreyandey11@gmail.com>
2025-04-22 01:27:25 +0530

    init and home page

---
+++ src/components/header.tsx
@@ -0,0 +1,59 @@
+import { ArrowUp, Moon, User } from "lucide-react";
+import { DM_Sans } from "next/font/google";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+});
+
+export default function Header() {
+  return (
+    <header
+      className={`flex items-center justify-between p-2 ${dmSans.variable}`}
+    >
+      <div className="flex gap-2 text-3xl font-bold">
+        <img src="/logo.png" alt="Logo" className="size-12" />
+        <span className="mt-4">Wabi Sabi</span>
+      </div>
+
+      <nav className="mt-4">
+        <ul className="flex items-center gap-10 text-3xl">
+          {navItems.map((item) => (
+            <li key={item.name}>
+              <a href={item.href} className="text-gray-700 hover:text-gray-900">
+                {item.name}
+              </a>
+            </li>
+          ))}
+          <div className="flex items-center gap-2">
+            <button className="flex items-center gap-1 rounded-full border-2 px-4 py-1 text-2xl">
+              <span>Purchase Plan</span>
+              <ArrowUp className="size-5 rotate-45" />
+            </button>
+            <button className="rounded-full border-2 p-2">
+              <User className="size-6" />
+            </button>
+            <button className="rounded-full border-2 p-2">
+              <Moon className="size-6" />
+            </button>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+const navItems = [
+  {
+    name: "Services",
+    href: "/",
+  },
+  {
+    name: "Works",
+    href: "/about",
+  },
+  {
+    name: "Feedbacks",
+    href: "/services",
+  },
+];
