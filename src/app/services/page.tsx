e5946b09a55ceba11e4167e4a5fe3acf9ba8bfca
Dey11 <shreyandey11@gmail.com>
2025-05-20 02:34:52 +0530

    make services page responsive

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -8,32 +8,30 @@ import { getCurrentPosters, ServiceType, services } from "@/lib/utils";
 export default function ServicesPage() {
   const [currIdx, setCurrIdx] = useState(0);
   const [currService, setCurrService] = useState<ServiceType["type"]>("banner");
-
   const currentPosters = getCurrentPosters(currService);
 
   function handleNext() {
     setCurrIdx((prev) => Math.min(prev + 1, currentPosters.length - 1));
   }
-
   function handlePrev() {
     setCurrIdx((prev) => Math.max(prev - 1, 0));
   }
 
   return (
-    <div className="grid h-[90svh] grid-cols-20 gap-6 pb-5">
-      <div className="col-span-8 flex flex-col justify-between pt-10 pb-20">
+    <div className="grid h-[90svh] grid-cols-10 gap-6 p-2 pb-5 lg:grid-cols-20">
+      <div className="col-span-10 flex flex-col justify-between pt-10 lg:col-span-8 lg:pb-20">
         <div className="flex flex-col gap-2">
           <p>101</p>
           <h1 className="text-2xl font-semibold">Our expertise</h1>
         </div>
 
-        <h2 className="text-secondary-foreground text-2xl leading-relaxed">
+        <h2 className="text-secondary-foreground hidden text-2xl leading-relaxed lg:block">
           Creative Solutions <br />
           <span className="font-semibold">
             Tailored To Your <br /> Brand's Vision
           </span>
         </h2>
-        <div className="flex items-center gap-3 text-2xl font-semibold">
+        <div className="hidden items-center gap-3 text-2xl font-semibold lg:flex">
           <button
             onClick={handlePrev}
             className="cursor-pointer rounded-2xl bg-[#FDF2F0] px-4 py-2"
@@ -49,8 +47,8 @@ export default function ServicesPage() {
         </div>
       </div>
 
-      <div className="text-secondary-foreground col-span-12 flex flex-col overflow-clip pt-10 pl-10">
-        <div className="mb-5 flex w-full items-center justify-end gap-5">
+      <div className="text-secondary-foreground col-span-10 flex flex-col overflow-clip lg:col-span-12 lg:pt-10 lg:pl-10">
+        <div className="mb-5 flex w-full flex-wrap items-center gap-3 sm:gap-5 lg:justify-end">
           {services.map((service, idx) => {
             const serviceType = service
               .toLowerCase()
@@ -62,10 +60,10 @@ export default function ServicesPage() {
                   setCurrService(serviceType);
                   setCurrIdx(0);
                 }}
-                className={`rounded-md px-4 py-1 text-xl transition-colors ${
+                className={`rounded-md px-2 py-1 transition-colors lg:text-xl ${
                   currService === serviceType
-                    ? "bg-[#CA207833] text-[#CA2078] hover:bg-[#CA207833]/70"
-                    : "bg-[#C7C7C7] hover:bg-[#c7c7c7]/70"
+                    ? "border bg-[#C7C7C7]/30 text-[#CA2078] hover:bg-[#CA207833]/70 lg:bg-[#CA207833]"
+                    : "text-secondary-foreground/80 border border-slate-300 hover:bg-[#c7c7c7]/70 lg:border-0 lg:bg-[#C7C7C7]"
                 }`}
               >
                 {service}
@@ -74,9 +72,9 @@ export default function ServicesPage() {
           })}
         </div>
         <motion.div
-          animate={{ x: `-${currIdx * 60}svh` }}
+          animate={{ x: `-${currIdx * 57}svh` }}
           transition={{ type: "spring", stiffness: 300, damping: 30 }}
-          className="flex items-center gap-5 rounded-3xl"
+          className="hidden items-center gap-5 rounded-3xl lg:flex"
         >
           {currentPosters.length > 0 ? (
             currentPosters.map((poster) => (
@@ -87,7 +85,7 @@ export default function ServicesPage() {
                 }}
                 key={poster.id}
               >
-                <div className="group absolute inset-0 z-10 rounded-3xl bg-gradient-to-b from-transparent from-30% to-180% transition-colors delay-100 hover:bg-gradient-to-b hover:to-white">
+                <div className="group absolute inset-0 z-10 rounded-3xl bg-gradient-to-b from-transparent from-30% to-180% transition-colors delay-100 hover:bg-gradient-to-b hover:to-black">
                   <div className="absolute top-5 right-5">
                     <MoveUpRight className="size-8 rounded-full border border-white bg-white p-2 opacity-0 transition-all delay-100 group-hover:opacity-100" />
                   </div>
@@ -105,6 +103,62 @@ export default function ServicesPage() {
             </div>
           )}
         </motion.div>
+        <motion.div
+          animate={{ x: `-${currIdx * 43}svh` }}
+          transition={{ type: "spring", stiffness: 300, damping: 30 }}
+          className="flex items-center gap-5 rounded-3xl lg:hidden"
+        >
+          {currentPosters.length > 0 ? (
+            currentPosters.map((poster) => (
+              <div
+                className="relative aspect-square h-[40svh] rounded-3xl bg-cover bg-center"
+                style={{
+                  backgroundImage: `url(${poster.src})`,
+                }}
+                key={poster.id}
+              >
+                <div className="group absolute inset-0 z-10 rounded-3xl bg-gradient-to-b from-transparent from-30% to-black to-180% transition-colors delay-100">
+                  <div className="absolute top-5 right-5">
+                    <MoveUpRight className="size-8 rounded-full border border-white bg-white p-2 transition-all delay-100" />
+                  </div>
+                  <div className="absolute bottom-0 max-w-sm rounded-3xl p-5 text-white transition-all delay-100">
+                    <p className="font-bold">{poster.title}</p>
+                    <p className="font-bold">{poster.type}</p>
+                    <p>{poster.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="flex h-[40svh] items-center justify-center lg:h-[60svh]">
+              <p>No images available for this service</p>
+            </div>
+          )}
+        </motion.div>
+      </div>
+      <div className="col-span-10 flex items-center justify-end gap-5 text-2xl font-semibold lg:hidden">
+        <button
+          onClick={handlePrev}
+          className="cursor-pointer rounded-2xl bg-[#FDF2F0] px-4 py-2"
+        >
+          {"<"}
+        </button>
+        <button
+          onClick={handleNext}
+          className="cursor-pointer rounded-2xl bg-[#FDF2F0] px-4 py-2"
+        >
+          {">"}
+        </button>
+      </div>
+
+      <div className="col-span-10 pb-5">
+        <h2 className="text-secondary-foreground text-lg leading-relaxed lg:hidden">
+          Creative Solutions
+          <span className="font-semibold">
+            {" "}
+            Tailored To Your Brand's Vision
+          </span>
+        </h2>
       </div>
     </div>
   );
