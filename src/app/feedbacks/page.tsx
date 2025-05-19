b1fc8dcc98238b7d1442c9c83a002aaa0570d5e9
Dey11 <shreyandey11@gmail.com>
2025-05-19 21:55:25 +0530

    make feedback page responsive

--- src/app/feedbacks/page.tsx
+++ src/app/feedbacks/page.tsx
@@ -2,13 +2,13 @@ import { Dices } from "lucide-react";
 
 export default function FeedbacksPage() {
   return (
-    <div className="grid h-[85dvh] grid-cols-20 gap-6">
-      <div className="col-span-8 flex flex-col justify-between py-20">
+    <div className="grid grid-cols-8 gap-6 p-2 sm:h-[85dvh] sm:grid-cols-20">
+      <div className="col-span-8 flex flex-col justify-between pt-10 sm:py-20">
         <div className="flex flex-col gap-2">
           <p>103</p>
           <h1 className="text-2xl font-semibold">Client's feedbacks</h1>
         </div>
-        <div className="text-secondary-foreground flex flex-col gap-8">
+        <div className="text-secondary-foreground hidden flex-col gap-8 sm:flex">
           <h2 className="text-2xl leading-relaxed">
             Authentic experiences shared <br />
             by those <span className="font-semibold">
@@ -21,7 +21,8 @@ export default function FeedbacksPage() {
           </button>
         </div>
       </div>
-      <div className="text-secondary-foreground col-span-12 grid grid-cols-2 gap-4 p-10">
+
+      <div className="text-secondary-foreground col-span-8 flex flex-col justify-center gap-4 sm:col-span-12 sm:grid sm:grid-cols-1 sm:pt-10 sm:pl-10 lg:grid-cols-2">
         {feedbackArray.map((feedback, idx) => (
           <FeedbackBox
             key={idx}
@@ -30,6 +31,16 @@ export default function FeedbacksPage() {
             where={feedback.where}
           />
         ))}
+      </div>
+      <div className="text-secondary-foreground col-span-8 flex-col gap-8 sm:hidden">
+        <h2 className="text-xl leading-relaxed">
+          Authentic experiences shared by those{" "}
+          <span className="font-semibold">who matter most</span> - our clients
+        </h2>
+
+        <button className="mt-2 flex w-fit items-center gap-2 rounded-full border-2 px-3 py-1 font-medium">
+          Let's shuffle <Dices className="size-5" />
+        </button>
       </div>
     </div>
   );
@@ -43,7 +54,7 @@ type FeedbackBoxProps = {
 
 export function FeedbackBox({ author, content, where }: FeedbackBoxProps) {
   return (
-    <div className="border-foreground flex aspect-square cursor-pointer flex-col justify-between gap-4 rounded-2xl border-2 p-4 transition-all duration-150 ease-in-out hover:border hover:shadow-2xl">
+    <div className="border-foreground flex cursor-pointer flex-col justify-between gap-4 rounded-2xl border-2 p-4 transition-all duration-150 ease-in-out hover:border hover:shadow-2xl lg:aspect-square">
       <p>{content}</p>
 
       <div className="flex items-center justify-end gap-2">
