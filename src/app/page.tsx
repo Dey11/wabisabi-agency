33ef5e9b5ce0a359d5d62ef4bb7534c0f6140c11
Dey11 <shreyandey11@gmail.com>
2025-05-19 18:06:40 +0530

    make homepage responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="grid h-[85dvh] grid-cols-20 gap-6 p-2 pt-5">
-      <div className="col-span-9 flex flex-col gap-4">
-        <div className="flex grow items-end justify-center rounded-2xl bg-[#EED1E3]">
-          <img src="logo.png" alt="Totoro" className="size-60" />
+    <main className="grid grid-cols-6 gap-4 p-2 py-5 sm:h-[85svh] sm:grid-cols-20 sm:gap-6">
+      <div className="order-2 col-span-6 flex flex-row-reverse gap-4 sm:order-1 sm:col-span-9 sm:flex-col">
+        <div className="flex items-end justify-center rounded-2xl bg-[#EED1E3] sm:grow">
+          <img
+            src="logo.png"
+            alt="Totoro"
+            className="size-[60svw] sm:size-60"
+          />
         </div>
 
-        <div className="flex gap-4">
+        <div className="flex grow flex-col justify-between gap-4 sm:grow-0 sm:flex-row">
           <div className="flex aspect-square h-auto w-full items-center justify-center rounded-2xl bg-[#FBFBFB]">
             <img src="icons/insta.svg" className="size-10" alt="Instagram" />
           </div>
@@ -20,7 +24,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="relative col-span-11">
+      <div className="relative order-1 col-span-6 h-[30svh] sm:order-2 sm:col-span-11 sm:h-auto">
         <Image
           priority
           fill
@@ -28,9 +32,22 @@ export default function Home() {
           alt="Description"
           className="rounded-2xl object-cover opacity-90"
         />
-
-        <div className="absolute inset-x-0 bottom-0 flex flex-col gap-8 rounded-b-xl bg-black/40 pt-5 pr-10 pb-10 text-end text-white">
+        <div className="absolute inset-x-0 bottom-0 hidden flex-col gap-8 rounded-b-xl bg-black/40 pt-5 pr-10 pb-10 text-end text-white sm:flex">
           <h3 className="text-4xl font-semibold">
+            Transforming Ideas Into <br />
+            Stunning Designs
+          </h3>
+
+          <p className="text-xl font-normal">
+            Empowering brands with creativity,
+            <br />
+            innovation, and visual storytelling
+          </p>
+        </div>
+      </div>
+      <div className="order-3 col-span-6 h-fit sm:hidden">
+        <div className="flex flex-col gap-4">
+          <h3 className="text-3xl font-semibold">
             Transforming Ideas Into <br />
             Stunning Designs
           </h3>
