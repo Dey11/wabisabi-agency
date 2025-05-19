74633b03e0b82b71e37a4b6b1a635ae19e53e72c
Dey11 <shreyandey11@gmail.com>
2025-05-20 02:41:28 +0530

    add most artwork instead of placeholders

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,14 +2,50 @@ export const BANNERS = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745771134/KANGA_RP_BANNER_suo8tb.png",
-    alt: "Kanga RP Banner",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747670787/playmakers_commission_akchx2.png",
+    alt: "Playmakers Commission Banner",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745770783/dune_commission_HD_z6aisw.png",
-    alt: "Dune Commission Banner",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747670533/RGS_Banner_yateby.png",
+    alt: "RGS Banner",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747660769/commission11_m5vqlh.png",
+    alt: "Commission Banner 11",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747660746/commission10_vfmyma.png",
+    alt: "Commission Banner 10",
+  },
+  {
+    id: 5,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747660549/RECON_RUST_BANNER_2_g5qp2m.png",
+    alt: "Recon Rust Banner",
+  },
+  {
+    id: 6,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747660547/DEVIOUS_NETWORK_SERVER_BANNER_tcbmoi.png",
+    alt: "Devious Network Server Banner",
+  },
+  {
+    id: 7,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747660477/commission4_bwm24a.png",
+    alt: "Commission Banner 4",
+  },
+  {
+    id: 8,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747660460/vortex_banner_HD_ppdy2l.png",
+    alt: "Vortex Banner",
   },
 ];
 
@@ -38,14 +74,74 @@ export const DISCORD_HEADERS = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745772239/commission21_smol_edited_ulikgw.png",
-    alt: "Commission 21",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671797/c61380134771067.61dc3ccaedb26_v8uaaq.png",
+    alt: "Discord Header 1",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745771960/Kinneyy_Profile_Banner_HD_b4qiok.png",
-    alt: "Kinneyy Profile Banner",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671785/commision_15_smol_2_y9drzt.png",
+    alt: "Commission Header 15",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671755/commission24_B_ou9soo.png",
+    alt: "Commission Header 24",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671728/commission20.2.1080_ypsneh.png",
+    alt: "Commission Header 20",
+  },
+  {
+    id: 5,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671686/icefuse_banner_dkglbl.jpg",
+    alt: "Icefuse Banner",
+  },
+  {
+    id: 6,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671674/justice_v3nxvk.jpg",
+    alt: "Justice Header",
+  },
+  {
+    id: 7,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671646/rust_to_riches_2_uvmaoy.jpg",
+    alt: "Rust to Riches Header",
+  },
+  {
+    id: 8,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671639/rustez_l4pndu.jpg",
+    alt: "RustEZ Header",
+  },
+  {
+    id: 9,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671621/unknown_kjbo7q.png",
+    alt: "Discord Header 9",
+  },
+  {
+    id: 10,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671619/unknown-58_gqrm1f.jpg",
+    alt: "Discord Header 10",
+  },
+  {
+    id: 11,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671604/wadadwd_qnbty1.png",
+    alt: "Discord Header 11",
+  },
+  {
+    id: 12,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671601/Untitled-2_hzk2ra.png",
+    alt: "Discord Header 12",
   },
 ];
 
@@ -53,14 +149,68 @@ export const LOGOS = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745771199/RUST_OUTLAWS_MASCOT_vrlwdo.png",
-    alt: "Rust Outlaws Mascot",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671655/rust_display2_ftialw.png",
+    alt: "Rust Display Logo",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745771198/JOOP_MASCOT_xtwrbb.jpg",
-    alt: "Joop Mascot",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671528/OCEAN_SIDE_RP_LOGO_TRANSPARENT_p3wbsr.png",
+    alt: "Ocean Side RP Logo",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671513/AQUA_MASCOT_rxr269.jpg",
+    alt: "Aqua Mascot",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671510/INFERNAL_MASCOT_ulkuez.jpg",
+    alt: "Infernal Mascot",
+  },
+  {
+    id: 5,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671507/RGS_BAN_MASCOT_ydnto6.png",
+    alt: "RGS Mascot",
+  },
+  {
+    id: 6,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671494/KIHU_MASCOT_xjxiex.jpg",
+    alt: "Kihu Mascot",
+  },
+  {
+    id: 7,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671489/YK_MASCOT_d8389n.jpg",
+    alt: "YK Mascot",
+  },
+  {
+    id: 8,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671487/SWUNG_MASCOT_z0ehx5.jpg",
+    alt: "Swung Mascot",
+  },
+  {
+    id: 9,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671485/SALTY_SCRAPLAND_MASCOT_xldabx.png",
+    alt: "Salty ScrapLand Mascot",
+  },
+  {
+    id: 10,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747670648/commission8_nsml5d.png",
+    alt: "Commission Logo 8",
+  },
+  {
+    id: 11,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747670487/Cruxio_1-trans_ycdar7.png",
+    alt: "Cruxio Logo",
   },
 ];
 
@@ -68,14 +218,134 @@ export const VIP_PACKS = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745771390/2_fcoope.png",
-    alt: "VIP Pack 2",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672768/rustmodel5_k0fwkl.png",
+    alt: "Rust Model 5",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1745771388/1_hszv0n.png",
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672763/rustmodel4_gptxup.png",
+    alt: "Rust Model 4",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672466/rustmodel3_xheb64.png",
+    alt: "Rust Model 3",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672379/rustmodel2_z4aclm.png",
+    alt: "Rust Model 2",
+  },
+  {
+    id: 5,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672367/rustmodel1_nj5qv3.png",
+    alt: "Rust Model 1",
+  },
+  {
+    id: 6,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672118/VIPs_1_2_hldpdv.png",
     alt: "VIP Pack 1",
+  },
+  {
+    id: 7,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672104/VIP_plus_2_cpeky0.png",
+    alt: "VIP Plus Pack",
+  },
+  {
+    id: 8,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672040/diamond_vip_2_wybnco.png",
+    alt: "Diamond VIP Pack",
+  },
+  {
+    id: 9,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747672027/golden_vip_2_s8jbdy.png",
+    alt: "Golden VIP Pack",
+  },
+  {
+    id: 10,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671979/titan_vip_2_sxicdp.png",
+    alt: "Titan VIP Pack",
+  },
+  {
+    id: 11,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671896/crate_10_jhnvda.png",
+    alt: "Crate 10",
+  },
+  {
+    id: 12,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671894/crate_4_ka4oru.png",
+    alt: "Crate 4",
+  },
+  {
+    id: 13,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671892/crate_3_pkkubo.png",
+    alt: "Crate 3",
+  },
+  {
+    id: 14,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671890/crate_2_vo8arh.png",
+    alt: "Crate 2",
+  },
+  {
+    id: 15,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671887/crate_1_tnr4ka.png",
+    alt: "Crate 1",
+  },
+  {
+    id: 16,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671885/crate_8_yb6niv.png",
+    alt: "Crate 8",
+  },
+  {
+    id: 17,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671883/crate_7_t6ee9s.png",
+    alt: "Crate 7",
+  },
+  {
+    id: 18,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671881/crate_5_joojrt.png",
+    alt: "Crate 5",
+  },
+  {
+    id: 19,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671879/crate_9_agajfj.png",
+    alt: "Crate 9",
+  },
+  {
+    id: 20,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671877/crate_6_xdwiwe.png",
+    alt: "Crate 6",
+  },
+  {
+    id: 21,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671593/zombe_model_rust_mvoxlh.png",
+    alt: "Zombe Model Rust",
+  },
+  {
+    id: 22,
+    image:
+      "https://res.cloudinary.com/dko5nf8tr/image/upload/v1747671539/RUST_VIP_ICONS_wrmxbf.png",
+    alt: "Rust VIP Icons",
   },
 ];
 
